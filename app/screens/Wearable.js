48d1a8e4f7ae6263c91855a45634da064663a380
sychiang1996 <32105215+sychiang1996@users.noreply.github.com>
2021-03-27 15:59:18 +0800

    Update Wearable.js
    Current state of Watch page. Best so far is can connect, but unable to read value

--- app/screens/Wearable.js
+++ app/screens/Wearable.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer, useIsFocused } from '@react-navigation/native';
 import {
   StyleSheet,
@@ -11,19 +11,236 @@ import {
   TouchableOpacity,
   BackHandler,
 } from 'react-native';
-import { BleManager } from 'react-native-ble-plx';
+import { BleManager, fullUUID } from 'react-native-ble-plx';
 
 function Wearable({ navigation }) {
-  console.log("page entered");
+  var [buttonText,setButtonText] = useState("Connect");
+  var [stillSamePage,inPage]=useState(false);
+  var [connected,testConn] = useState(false);
   var focused=useIsFocused();
-  const manager=new BleManager();
-  console.log('New BleManager created');
-  console.log("focused value is",focused);
+  var bluetoothDevice=useState();
+  var deviceID;
+  if (!stillSamePage){
+    console.log("Watch page entered");
+    stillSamePage=inPage(true);
+    console.log('New BleManager created');
+    return manager=new BleManager();
+  }
+  console.log("'focused' variable value is",focused);
+  //console.log(manager,buttonText,stillSamePage,connected);
   if (!focused){
     manager.destroy();
     console.log('BleManager destroyed');
-    console.log("page exited");
+    console.log("Watch page exited");
   }
+  console.log(buttonText,stillSamePage,connected);
+  console.log("Bluetooth device:",bluetoothDevice);
+  //console.log("Bluetooth device services:",bluetoothDevice.discoverAllServicesAndCharacteristics());
+  async function scanAndConnect() {//edited from RoomLightBLE
+    console.log('Scanning...');
+    manager.startDeviceScan(null, null, (error, device) => {
+      console.log(device);
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (device.name === 'Bluefruit HRM') {
+        console.log('Device found!');
+        manager.stopDeviceScan();
+        bluetoothDevice=device;
+        deviceID=device.id
+        //console.log('device.id:',device.id)
+        console.log('deviceID=device.id=',deviceID)
+        console.log('device:',device)
+        device.connect()
+        .then((device) => {
+          return device.discoverAllServicesAndCharacteristics();
+        }).then(() => {
+          console.log('Device connected.');
+        }).catch((error) => {
+          console.log(error);
+        });
+        return bluetoothDevice;
+      }
+    })
+  }
+
+  async function getData(){//edited/taken from RoomLightBLE
+    //manager.readCharacteristicForDevice(deviceID,'0000180d-0000-1000-8000-00805f9b34fb',fullUUID('2a37'))
+    manager.readCharacteristicForService(deviceID,'074fbcce-b336-419e-b350-7455450a6f2c','074f0001-b336-419e-b350-7455450a6f2c')
+    .then((response)=>{
+      console.log(response);
+      return response;
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  async function stopScan() {//edited/taken from RoomLightBLE
+    console.log('Stopping...');
+    manager.cancelDeviceConnection(deviceID)
+    .then(() => {
+      console.log('Disconnected.');
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+  
+  async function dataFlow(){
+    console.log('Scanning...');
+    manager.startDeviceScan(null, null, (error, device) => {
+      console.log(device);
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (device.name === 'Bluefruit HRM') {
+        console.log('Device found!');
+        manager.stopDeviceScan();
+        bluetoothDevice=device;
+        deviceID=device.id
+        //console.log('device.id:',device.id)
+        console.log('deviceID=device.id=',deviceID)
+        console.log('device:',device)
+        device.connect()
+        .then((device) => {
+          return device.discoverAllServicesAndCharacteristics();
+        }).then(() => {
+          console.log('Device connected.');
+        }).catch((error) => {
+          console.log(error);
+        });
+        //return bluetoothDevice;
+      }
+    }).then(()=>{
+      //manager.readCharacteristicForDevice(deviceID,'0000180d-0000-1000-8000-00805f9b34fb',fullUUID('2a37'))
+      manager.readCharacteristicForService(deviceID,'074fbcce-b336-419e-b350-7455450a6f2c','074f0001-b336-419e-b350-7455450a6f2c')
+      .then((response)=>{
+        console.log(response);
+        return response;
+      }).catch((error) => {
+        console.log(error);
+      });
+    }).catch((error) => {
+      console.log(error);
+    }).then(()=>{
+      console.log('Stopping...');
+      manager.cancelDeviceConnection(deviceID)
+      .then(() => {
+        console.log('Disconnected.');
+      }).catch((error) => {
+        console.log(error);
+      });
+    })
+  }
+
+  async function scanConnectGetDisconnTry() {//Latest attempt, not really working
+    manager.startDeviceScan(null, null, (error, device) => {
+      console.log(device);
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (device.name === 'Bluefruit HRM') {
+        console.log('Device found!');
+        manager.stopDeviceScan();
+        bluetoothDevice = device;
+        device.connect()
+        .then(async (device) => {
+          console.log('Device connected.');
+          return await device.discoverAllServicesAndCharacteristics();
+          const services= await device.services();
+          //return services
+        }).then(()=>{
+          bluetoothDevice.readCharacteristicForService('074fbcce-b336-419e-b350-7455450a6f2c','074f0001-b336-419e-b350-7455450a6f2c')
+            .then((response)=>{
+            console.log(response);
+            return response;
+            })
+        }).then(()=>{
+          console.log('Stopping...');
+          manager.cancelDeviceConnection(deviceID)
+          .then(() => {
+            console.log('Disconnected.');
+          }).catch((error) => {
+            console.log(error);
+          });
+        });
+      }
+    })
+  }
+  
+  async function scanConnectGetDisconn() {//Works up until trying to read a value
+    console.log('Scanning...');
+    manager.startDeviceScan(null, null, (error, device) => {
+      console.log(device);
+      if (error) {
+        console.log(error);
+        return;
+      }
+      if (device.name === 'Bluefruit HRM') {
+        console.log('Device found!');
+        manager.stopDeviceScan();
+        bluetoothDevice=device;
+        deviceID=device.id
+        //console.log('device.id:',device.id)
+        console.log('deviceID=device.id=',deviceID)
+        //console.log('device:',device)
+        device.connect()
+        .then(async (device) => {
+          //return manager.discoverAllServicesAndCharacteristicsForDevice(device.id)
+          return await device.discoverAllServicesAndCharacteristics();
+        }).then(() => {
+            console.log('Device connected.');
+            //bluetoothDevice.discoverAllServicesAndCharacteristics().then((scan)=>{console.log(scan)}).catch((error)=>{console.log(error)});
+            //manager.servicesForDevice(deviceID).then((scan)=>{console.log(scan)}).catch((error)=>{console.log(error)});
+            console.log(deviceID);
+            //manager.descriptorsForDevice(deviceID,'0000180d-0000-1000-8000-00805f9b34fb',fullUUID('2a37'))
+            //manager.readCharacteristicForDevice(deviceID,'0000180d-0000-1000-8000-00805f9b34fb',fullUUID('3131'))
+            //manager.readCharacteristicForDevice(deviceID,'074fbcce-b336-419e-b350-7455450a6f2c','074f0001-b336-419e-b350-7455450a6f2c')
+            //bluetoothDevice.writeCharacteristicWithResponseForService('0000180d-0000-1000-8000-00805f9b34fb',fullUUID('2a37'), "AQ==")
+            //bluetoothDevice.monitorCharacteristicForService('0000180d-0000-1000-8000-00805f9b34fb',fullUUID('2a37'),(error,response)=>{})
+            //return bluetoothDevice.monitorCharacteristicForService('074fbcce-b336-419e-b350-7455450a6f2c','074f0001-b336-419e-b350-7455450a6f2c')
+            //manager.readCharacteristicForDevice(deviceID,'074fbcce-b336-419e-b350-7455450a6f2c','074f0001-b336-419e-b350-7455450a6f2c')
+            return device.readCharacteristicForService('074fbcce-b336-419e-b350-7455450a6f2c','074f0001-b336-419e-b350-7455450a6f2c')})
+            .then((response)=>{
+            console.log(response);
+            return response;
+            }).then(()=>{
+          console.log('Stopping...');
+          manager.cancelDeviceConnection(deviceID)
+          .then(() => {
+            console.log('Disconnected.');
+          }).catch((error) => {
+            console.log(error);
+          });
+        }).catch((error) => {
+          console.log(error);
+          console.log('Stopping due to error...');
+          manager.cancelDeviceConnection(deviceID)
+          .then(() => {
+            console.log('Disconnected.');
+          }).catch((error) => {
+            console.log(error);
+          });
+          })
+        /*})*/.catch((error) => {
+          console.log(error);
+        });
+        //return bluetoothDevice;
+      }
+    })
+    }
+
+  async function dataGetFlow(){//Outright just putting the functions togetrher
+    console.log("step:scanning and connecting");
+    await scanAndConnect();
+    console.log("step:getting data");
+    await getData();
+    console.log("disconnecting");
+    await stopScan();
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profileArea}>
@@ -45,7 +262,10 @@ function Wearable({ navigation }) {
         <View style={styles.dataAreaBorderPadding}></View>
         <TouchableOpacity
         style={[styles.buttonStyle, {justifyContent: 'flex-end', alignSelf: 'flex-end'}]}
-        onPress={null}
+        //onPress={scanConnectGetDisconn}//works up to before value read
+        onPress={scanConnectGetDisconnTry}//tried
+        //onPress={dataFlow}//nope
+        //onPress={dataGetFlow}//nope
         >
           <Text>Measure</Text>
         </TouchableOpacity>
@@ -67,15 +287,16 @@ function Wearable({ navigation }) {
       <View style={styles.buttonArea}>
         <TouchableOpacity
         style={styles.buttonStyle}
-        onPress={null}
+        onPress={()=>[testConn(!connected),
+          !connected? (scanAndConnect(),setButtonText("Disconnect")):(stopScan(),setButtonText("Connect"))]}
         >
-          <Text style={styles.bottomButtonFont}>Sync Watch</Text>
+          <Text style={styles.bottomButtonFont}>{buttonText}</Text>
         </TouchableOpacity>
         <TouchableOpacity
-        style={styles.buttonStyle}
+        style={[styles.buttonStyle,{flexShrink:1}]}
         onPress={null}
         >
-          <Text style={styles.bottomButtonFont}>Sync Data</Text>
+          <Text style={[styles.bottomButtonFont,{alignContent:"center"}]}>Send data</Text>
         </TouchableOpacity>
         <TouchableOpacity
         style={styles.buttonStyle}
@@ -173,10 +394,12 @@ const styles = StyleSheet.create({
         backgroundColor:'#E29578',
         borderRadius:10,
         borderWidth: 1,
-        borderColor: '#fff'
+        borderColor: '#fff',
+        alignItems: "center"
       },
     bottomButtonFont:{
-        fontSize:20
+        fontSize:20,
+        justifyContent: "center"
     }
 });
 
